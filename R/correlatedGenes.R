3752f5ff4ea8def374231e27a04b0f28ce8b24a6
diazdc <walktheline@gmail.com>
2020-03-09 16:16:18 -0500

    added correlatedGenes.R

---
+++ R/correlatedGenes.R
@@ -0,0 +1,21 @@
+corrleatedGenes <- function(seurat_obj, goi,
+  cell_type = unique(Idents(seurat_obj)), condition = "homeo", n_results = 200) {
+  
+  if (length(goi) > 1) {
+    stop("Please enter a single gene")
+  }
+
+  mat <- seurat_obj@assays$RNA@data
+  mat_raw <- as.matrix(mat)
+  
+  mat_sub <- mat_raw[, seurat_obj[["data.set"]][,1] %in% condition &
+    seurat_obj[['cell.type.ident']][,1] %in% cell_type]
+
+  gene <- as.numeric(mat_sub[goi,])
+  cor_vals <- apply(mat_sub, 1, function(x) {cor(gene,x)})
+  cor_vals <- sort(cor_vals, decreasing = TRUE)
+  cor_vals <- c(head(cor_vals, n_results), tail(cor_vals, n_results))
+  
+  return(data.frame(Gene.name.uniq = names(cor_vals),
+      correlation_coeff = cor_vals))
+}
--- R/mcDiffTreatments.R
+++ R/mcDiffTreatments.R
@@ -1,5 +1,5 @@
 # ================ Diff expression for every treatment with NEW Cell classes
-{
+if (FALSE) {
   if (DefaultAssay(obj_integrated) != "RNA") {
     print("Changing default assay to RNA")
     DefaultAssay(obj_integrated) <- "RNA"
@@ -101,173 +101,176 @@
 }
 
 # ================ Plot results from above (each cell type vs all timepoints)
-cell_specific <- TRUE
-if (cell_specific) {
-  file_name <- "MarkerList_specific_"
-  all_markers <- readRDS(dataPath(paste0(file_name, script_name,"_.RDS")))
-  folder <- "diff_exp_cell_specific/"
-} else {
-  file_name <- "MarkerList_"
-  all_markers <- readRDS(dataPath(paste0(file_name, script_name,"_.RDS")))
-  folder <- "diff_exp_by_cell_type/"
-}
+if (FALSE) { # place holder for new function
+  cell_specific <- TRUE
+  if (cell_specific) {
+    file_name <- "MarkerList_specific_"
+    all_markers <- readRDS(dataPath(paste0(file_name, script_name,"_.RDS")))
+    folder <- "diff_exp_cell_specific/"
+  } else {
+    file_name <- "MarkerList_"
+    all_markers <- readRDS(dataPath(paste0(file_name, script_name,"_.RDS")))
+    folder <- "diff_exp_by_cell_type/"
+  }
 
-table(all_markers$cell.type.and.trt) < 20
-length(all_markers$cell.type.and.trt)
+  table(all_markers$cell.type.and.trt) < 20
+  length(all_markers$cell.type.and.trt)
 
-short_sig_figs <- TRUE
-if (short_sig_figs) {
-  obj_integrated@meta.data$cell.type.and.trt <- paste0(
-    obj_integrated@meta.data$cell.type.ident, "_",
-    obj_integrated@meta.data$data.set)
+  short_sig_figs <- TRUE
+  if (short_sig_figs) {
+    obj_integrated@meta.data$cell.type.and.trt <- paste0(
+      obj_integrated@meta.data$cell.type.ident, "_",
+      obj_integrated@meta.data$data.set)
 
-  # Shortening sig figs for plotting
-  all_markers$avg_logFC <- signif(all_markers$avg_logFC, 3)
-  all_markers$p_val <- signif(all_markers$p_val, 3)
-  all_markers$pct.ratio <- signif(all_markers$pct.ratio, 2)
-  all_markers$pct.1 <- signif(all_markers$pct.1, 3)
-  all_markers$pct.2 <- signif(all_markers$pct.2, 3)
+    # Shortening sig figs for plotting
+    all_markers$avg_logFC <- signif(all_markers$avg_logFC, 3)
+    all_markers$p_val <- signif(all_markers$p_val, 3)
+    all_markers$pct.ratio <- signif(all_markers$pct.ratio, 2)
+    all_markers$pct.1 <- signif(all_markers$pct.1, 3)
+    all_markers$pct.2 <- signif(all_markers$pct.2, 3)
 
-  DefaultAssay(obj_integrated) <- "RNA"
-  assay <- "assay_RNA_"
+    DefaultAssay(obj_integrated) <- "RNA"
+    assay <- "assay_RNA_"
+  }
 }
 
 
 # ---- Get summary stats for all diff genes  ---- #
-summary_stat <- FALSE
-if (summary_stat) {
-  log_mat <- obj_integrated@assays$RNA@data
+if (FALSE) { # place holder for new function
+  summary_stat <- FALSE
+  if (summary_stat) {
+    log_mat <- obj_integrated@assays$RNA@data
 
-  gene_marker_dist <- function(i, group1) {
-    if (group1 == TRUE) {bc_ind <- grepl(
-      all_markers$cell.type.and.trt[i], meta$cell.type.and.trt)
-      group <- "group-1"
-    } else {bc_ind <- !grepl(
-      all_markers$cell.type.and.trt[i], meta$cell.type.and.trt)
-      group <- "group-2"}
+    gene_marker_dist <- function(i, group1) {
+      if (group1 == TRUE) {bc_ind <- grepl(
+        all_markers$cell.type.and.trt[i], meta$cell.type.and.trt)
+        group <- "group-1"
+      } else {bc_ind <- !grepl(
+        all_markers$cell.type.and.trt[i], meta$cell.type.and.trt)
+        group <- "group-2"}
+      
+      gene_vec <- log_mat[all_markers$Gene.name.uniq[i], bc_ind]
+      
+      stats <- summary(log_mat[all_markers$Gene.name.uniq[i], bc_ind])
+      stats <- c(paste(all_markers$Gene.name.uniq[i], group),
+        paste(names(stats), signif(unname(stats), 2), sep = ":"))
+      return(stats)
+    }
     
-    gene_vec <- log_mat[all_markers$Gene.name.uniq[i], bc_ind]
-    
-    stats <- summary(log_mat[all_markers$Gene.name.uniq[i], bc_ind])
-    stats <- c(paste(all_markers$Gene.name.uniq[i], group),
-      paste(names(stats), signif(unname(stats), 2), sep = ":"))
-    return(stats)
+    # This takes awhile to run for the macrophage data, even with 10 cores
+    stat_sum1 <- parallel::mclapply(1:nrow(all_markers),
+      gene_marker_dist, group1 = TRUE, mc.cores = 10)
+
+    stat_sum2 <- parallel::mclapply(1:nrow(all_markers),
+      gene_marker_dist, group1 = FALSE, mc.cores = 10)
+
+    save(stat_sum1, stat_sum2,
+      file = dataPath(paste0("stat_sums", script_name, "_.RData")))
+    load(file = dataPath(paste0("stat_sums", script_name, "_.RData")))
   }
-  
-  # This takes awhile to run for the macrophage data, even with 10 cores
-  stat_sum1 <- parallel::mclapply(1:nrow(all_markers),
-    gene_marker_dist, group1 = TRUE, mc.cores = 10)
 
-  stat_sum2 <- parallel::mclapply(1:nrow(all_markers),
-    gene_marker_dist, group1 = FALSE, mc.cores = 10)
+  # get index change for cell type and treatment
+  ind_chng <- match(unique(all_markers$cell.type.and.trt),
+    all_markers$cell.type.and.trt)
 
-  save(stat_sum1, stat_sum2,
-    file = dataPath(paste0("stat_sums", script_name, "_.RData")))
-  load(file = dataPath(paste0("stat_sums", script_name, "_.RData")))
+
+  # ---- Violin Plot panels ---- #
+  dir.create(figurePath(paste0(folder, "Vplot-top-markers-by-treatment/")))
+  n_genes <- 200
+  seq_nums <- seq(1, n_genes, by = 20)
+  meta <- obj_integrated@meta.data
+
+  parallel::mclapply(seq_along(ind_chng), mc.cores = 10, function (i) {
+    ifelse(seq_along(ind_chng[i]) == tail(seq_along(ind_chng),1),
+      ind_range <- ind_chng[i]:((ind_chng[i + 1]) - 1),
+      ind_range <- ind_chng[i]:nrow(all_markers))
+    
+    population <- paste0(all_markers$cell.type.and.trt[ind_range], " vs. ",
+      (strsplit(all_markers$cell.type.and.trt[ind_range],"-")[[1]][1]), "-other")
+    
+    stats <- paste0("p-value: ", all_markers$p_val[ind_range], ", ",
+      "avg. logFC: ", all_markers$avg_logFC[ind_range], ", ",
+      "pct.1: ", all_markers$pct.1[ind_range], ", ",
+      "pct.2: ", all_markers$pct.2[ind_range], ", ",
+      "pct.ratio: ", all_markers$pct.ratio[ind_range])
+    
+    genes <- all_markers$Gene.name.uniq[ind_range]
+    genes <- genes[1:n_genes]
+
+    for(j in seq_along(seq_nums)) {
+      cell_ident <- gsub("_.*","", all_markers$cell.type.and.trt[ind_range][1])
+
+      sub_ind <- seq_nums[j]:(seq_nums[j]+19)
+      pop_sub <- population[sub_ind]
+      stats_sub <- stats[sub_ind]
+
+      to_plot <- genes[sub_ind]
+      if (NA %in% to_plot) {break}
+
+      v <- VlnPlot(obj_integrated, to_plot, pt.size = 0.25,
+        idents = cell_ident, cols = trt_colors,
+        combine = FALSE, group.by = "data.set")
+      for(k in seq_along(v)) {
+        v[[k]] <- v[[k]] + NoLegend() + labs(
+          caption = paste(pop_sub[k], "\n", stats_sub[k])) +
+        theme(plot.caption = element_text(hjust = 0))
+      }
+
+      path <- figurePath(paste0(folder, "Vplot-top-markers-by-treatment/",
+        all_markers$cell.type.and.trt[ind_chng[i]], "_top_", seq_nums[j],
+        "-", (seq_nums[j] + 19),"_features.png"))
+
+      png(path, width = 30, height = 25, units = "in", res = 200)
+      print(cowplot::plot_grid(plotlist = v))
+      dev.off()
+    }
+  })
+
+
+  # ---- Feature Plot panels ---- #
+  dir.create(figurePath(paste0(folder, "Fplot-top-markers-treatment/")))
+  n_genes <- 200
+  seq_nums <- seq(1, n_genes, by = 20)
+
+  parallel::mclapply(seq_along(ind_chng), mc.cores = 12, function (i) {
+    ifelse(seq_along(ind_chng[i]) == tail(seq_along(ind_chng),1),
+      ind_range <- ind_chng[i]:((ind_chng[i + 1]) - 1),
+      ind_range <- ind_chng[i]:nrow(all_markers))
+    
+    population <- paste0(all_markers$cell.type.and.trt[ind_range], " vs. ",
+      (strsplit(all_markers$cell.type.and.trt[ind_range],"-")[[1]][1]), "-other")
+    
+    stats <- paste0("p-val: ", all_markers$p_val[ind_range], ", ",
+      "avg. logFC: ", all_markers$avg_logFC[ind_range], ", ",
+      "pct.1: ", all_markers$pct.1[ind_range], ", ",
+      "pct.2: ", all_markers$pct.2[ind_range], ", ",
+      "pct.ratio: ", all_markers$pct.ratio[ind_range])
+
+    genes <- all_markers$Gene.name.uniq[ind_range]
+    genes <- genes[1:n_genes]
+
+    for(j in seq_along(seq_nums)) {
+      sub_ind <- seq_nums[j]:(seq_nums[j] + 19)
+      to_plot <- genes[sub_ind]
+      pop_sub <- population[sub_ind]
+      stats_sub <- stats[sub_ind]
+      
+      if (NA %in% to_plot) {break}
+      f <- FeaturePlot(obj_integrated, to_plot,
+        reduction = "umap", pt.size = 0.25, combine = FALSE)
+      
+      for(k in seq_along(f)) {
+        f[[k]] <- f[[k]] + NoLegend() + NoAxes() + labs(
+          caption = paste(pop_sub[k], "\n", stats_sub[k])) +
+        theme(plot.caption = element_text(hjust = 0))
+      }
+
+      path <- figurePath(paste0(folder, "Fplot-top-markers-treatment/",
+        all_markers$cell.type.and.trt[ind_chng[i]], "_top_", seq_nums[j],
+        "-", (seq_nums[j] + 19),"_features.png"))
+      png(path, width = 30, height = 25, units = "in", res = 200)
+      print(cowplot::plot_grid(plotlist = f))
+      dev.off()
+    }
+  })
 }
-
-# get index change for cell type and treatment
-ind_chng <- match(unique(all_markers$cell.type.and.trt),
-  all_markers$cell.type.and.trt)
-
-
-# ---- Violin Plot panels ---- #
-dir.create(figurePath(paste0(folder, "Vplot-top-markers-by-treatment/")))
-n_genes <- 200
-seq_nums <- seq(1, n_genes, by = 20)
-meta <- obj_integrated@meta.data
-
-parallel::mclapply(seq_along(ind_chng), mc.cores = 10, function (i) {
-  ifelse(seq_along(ind_chng[i]) == tail(seq_along(ind_chng),1),
-    ind_range <- ind_chng[i]:((ind_chng[i + 1]) - 1),
-    ind_range <- ind_chng[i]:nrow(all_markers))
-  
-  population <- paste0(all_markers$cell.type.and.trt[ind_range], " vs. ",
-    (strsplit(all_markers$cell.type.and.trt[ind_range],"-")[[1]][1]), "-other")
-  
-  stats <- paste0("p-value: ", all_markers$p_val[ind_range], ", ",
-    "avg. logFC: ", all_markers$avg_logFC[ind_range], ", ",
-    "pct.1: ", all_markers$pct.1[ind_range], ", ",
-    "pct.2: ", all_markers$pct.2[ind_range], ", ",
-    "pct.ratio: ", all_markers$pct.ratio[ind_range])
-  
-  genes <- all_markers$Gene.name.uniq[ind_range]
-  genes <- genes[1:n_genes]
-
-  for(j in seq_along(seq_nums)) {
-    cell_ident <- gsub("_.*","", all_markers$cell.type.and.trt[ind_range][1])
-
-    sub_ind <- seq_nums[j]:(seq_nums[j]+19)
-    pop_sub <- population[sub_ind]
-    stats_sub <- stats[sub_ind]
-
-    to_plot <- genes[sub_ind]
-    if (NA %in% to_plot) {break}
-
-    v <- VlnPlot(obj_integrated, to_plot, pt.size = 0.25,
-      idents = cell_ident, cols = trt_colors,
-      combine = FALSE, group.by = "data.set")
-    for(k in seq_along(v)) {
-      v[[k]] <- v[[k]] + NoLegend() + labs(
-        caption = paste(pop_sub[k], "\n", stats_sub[k])) +
-      theme(plot.caption = element_text(hjust = 0))
-    }
-
-    path <- figurePath(paste0(folder, "Vplot-top-markers-by-treatment/",
-      all_markers$cell.type.and.trt[ind_chng[i]], "_top_", seq_nums[j],
-      "-", (seq_nums[j] + 19),"_features.png"))
-
-    png(path, width = 30, height = 25, units = "in", res = 200)
-    print(cowplot::plot_grid(plotlist = v))
-    dev.off()
-  }
-})
-
-
-# ---- Feature Plot panels ---- #
-dir.create(figurePath(paste0(folder, "Fplot-top-markers-treatment/")))
-n_genes <- 200
-seq_nums <- seq(1, n_genes, by = 20)
-
-parallel::mclapply(seq_along(ind_chng), mc.cores = 12, function (i) {
-  ifelse(seq_along(ind_chng[i]) == tail(seq_along(ind_chng),1),
-    ind_range <- ind_chng[i]:((ind_chng[i + 1]) - 1),
-    ind_range <- ind_chng[i]:nrow(all_markers))
-  
-  population <- paste0(all_markers$cell.type.and.trt[ind_range], " vs. ",
-    (strsplit(all_markers$cell.type.and.trt[ind_range],"-")[[1]][1]), "-other")
-  
-  stats <- paste0("p-val: ", all_markers$p_val[ind_range], ", ",
-    "avg. logFC: ", all_markers$avg_logFC[ind_range], ", ",
-    "pct.1: ", all_markers$pct.1[ind_range], ", ",
-    "pct.2: ", all_markers$pct.2[ind_range], ", ",
-    "pct.ratio: ", all_markers$pct.ratio[ind_range])
-
-  genes <- all_markers$Gene.name.uniq[ind_range]
-  genes <- genes[1:n_genes]
-
-  for(j in seq_along(seq_nums)) {
-    sub_ind <- seq_nums[j]:(seq_nums[j] + 19)
-    to_plot <- genes[sub_ind]
-    pop_sub <- population[sub_ind]
-    stats_sub <- stats[sub_ind]
-    
-    if (NA %in% to_plot) {break}
-    f <- FeaturePlot(obj_integrated, to_plot,
-      reduction = "umap", pt.size = 0.25, combine = FALSE)
-    
-    for(k in seq_along(f)) {
-      f[[k]] <- f[[k]] + NoLegend() + NoAxes() + labs(
-        caption = paste(pop_sub[k], "\n", stats_sub[k])) +
-      theme(plot.caption = element_text(hjust = 0))
-    }
-
-    path <- figurePath(paste0(folder, "Fplot-top-markers-treatment/",
-      all_markers$cell.type.and.trt[ind_chng[i]], "_top_", seq_nums[j],
-      "-", (seq_nums[j] + 19),"_features.png"))
-    png(path, width = 30, height = 25, units = "in", res = 200)
-    print(cowplot::plot_grid(plotlist = f))
-    dev.off()
-  }
-})
-
